d63c3ad9902c68cc2f202095890482bcf738bf70
sallarahulreddy <30665564+sallarahulreddy@users.noreply.github.com>
2017-08-09 07:28:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+var img=document.getElementById('madi');
+
+function moveRight()
+{ var marginleft=marginleft+10;
+   marginleft=marginleft+'px';
+}
+
+img.click(function(){
+var interval=setInterval(moveRight,100);
+});
